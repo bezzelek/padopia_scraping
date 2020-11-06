d6939037d180c71e0846d5cd9de976ea159c763a
Bogdan Sikorsky <bogdan.sikorsky@tuta.io>
2020-11-06 12:23:00 +0200

    [add] basic docker for deploy

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.8.3-alpine3.12
+
+ENV PYTHONPATH="$PYTHONPATH:/app/src" \
+    FLASK_ENV="development"
+
+COPY ["requirements.txt", "/app/"]
+
+
+RUN apk add --update --no-cache make bash libpq
+RUN /usr/local/bin/python -m pip install --upgrade pip
+RUN apk add --update --no-cache g++ gcc libxslt-dev
+RUN apk add --no-cache --virtual build \
+    build-base \
+    musl-dev \
+    libffi-dev \
+    openssl-dev \
+    && pip install --no-cache-dir -r /app/requirements.txt \
+    && pip install --no-cache-dir pymongo[srv] \
+    && pip install --no-cache-dir pymongo[tls] \
+    && apk del build
+
+WORKDIR /app
+EXPOSE 5000
--- src/webscraper/spiders/runner.py
+++ src/webscraper/spiders/runner.py
@@ -1,16 +1,24 @@
 """runner"""
-from apscheduler.schedulers.twisted import TwistedScheduler
+import os
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 from src.webscraper.spiders.ireland_spider import IrelandSpider
 
+
+class Scraper:
+    def __init__(self):
+        settings_file_path = 'webscraper.settings'
+        os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_file_path)
+        self.process = CrawlerProcess(get_project_settings())
+        self.spider = IrelandSpider
+
+    def run_spiders(self):
+        self.process.crawl(self.spider)
+        self.process.start()
+
+
 if __name__ == "__main__":
-    process = CrawlerProcess(get_project_settings())
-    """Scheduler to run task periodically"""
-    scheduler = TwistedScheduler()
-    scheduler.add_job(process.crawl, 'interval', args=[IrelandSpider], seconds=60*60*24)
-    scheduler.start()
-    process.start(False)
-    # process.crawl(IrelandSpider)
-    # process.start()
+    scraper = Scraper()
+    scraper.run_spiders()
