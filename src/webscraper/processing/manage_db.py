aad9db53cf141df02c0d624b8dccc0b4dff1db9d
bogdan.sikorsky <bogdan.sikorsky@tuta.io>
2021-02-10 10:34:58 +0200

    [add] converter for all non-eur currencies in db

---
+++ src/webscraper/processing/manage_db.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+from src.webscraper.settings import DB_CONNECTION
+
+
+def manage_db():
+
+    """Connecting to DB"""
+    connection = MongoClient(DB_CONNECTION)
+    db = connection['padopiadata']
+    collection_property = db['test_property']
+
+    """Manage DB"""
+    test = collection_property.update_many(
+        {}, {"$unset": {"property_price": 1}}
+    )
+
+
+if __name__ == "__main__":
+    manage_db()
---
+++ src/webscraper/processing/prices/currency_convertation.py
@@ -0,0 +1,39 @@
+"""Script that finds properties that don't have price converted to EUR currency"""
+
+
+from webscraper.processing.prices.helpers import get_db_connection, get_exchange_rate, processing_documents
+
+
+def convert_currency():
+    """DB connection"""
+    db = get_db_connection()
+    collection_property = db['property']
+
+    """Search details"""
+    query = {
+        '$and': [
+            {'property_price.currency_iso': {'$exists': False}},
+            {'property_cost_currency': {'$exists': True}},
+        ]
+    }
+    purpose_type = True
+    sort = 'date_time', -1
+
+    """Getting number of properties without accurate address"""
+    number_of_docs = collection_property.count_documents(query)
+
+    """Log"""
+    print(f'Work started! There are {number_of_docs} records to process...')
+
+    """Getting currencies exchange rates"""
+    exchange_rates = get_exchange_rate('EUR')
+
+    """Processing documents"""
+    result = processing_documents(query, number_of_docs, exchange_rates, sort, purpose_type)
+
+    """Log"""
+    print(f'Job done! {result} records processed of {number_of_docs}.')
+
+
+if __name__ == "__main__":
+    convert_currency()
